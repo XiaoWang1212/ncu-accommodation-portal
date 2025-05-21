8cff589452ad458f868626e756092b9cdbc9cac9
XiaoWang1212 <danny20041212@gmail.com>
2025-05-21 18:23:00 +0800

    update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,6 @@
 from app import create_app, socketio
-import threading
 
 app = create_app()
 
-def run_socketio():
-    print("start up WebSocket...")
-    socketio.run(app, debug=False, allow_unsafe_werkzeug=True)
-
 if __name__ == '__main__':
-    # 啟動 WebSocket 於獨立執行緒
-    thread = threading.Thread(target=run_socketio)
-    thread.daemon = True  # 設定為守護執行緒，主程式結束時自動結束
-    thread.start()
-    
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
